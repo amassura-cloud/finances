4a7b5d5a93c9a81c3db0aa5085034291ace006ea
amassura-cloud <amassura@gmail.com>
2026-06-23 23:28:33 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="painel-v39";
+const CACHE="painel-v41";
 self.addEventListener("install",e=>self.skipWaiting());
 self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
 self.addEventListener("fetch",e=>{
